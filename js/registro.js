17b98b75c8c8be36dff3befee7ae5b81c981b508
juan pablo <chito.pepsi@gmail.com>
2023-04-24 18:10:12 -0600

    validaciones bordes y lista de errores

--- js/registro.js
+++ js/registro.js
@@ -1,98 +1,162 @@
 let nombre = document.getElementById("name");
-console.log(nombre);
 let apellido = document.getElementById("Nombre");
-console.log(apellido);
 let txtTelefono = document.getElementById("telefono");
-console.log(txtTelefono);
 let email = document.getElementById("email");
-console.log(email);
-let boleta=document.getElementById("boleta");
-console.log(boleta);
-
+let boleta = document.getElementById("boleta");
 
 let btnConfirm = document.getElementById("btnConfirm");
 
-// Función principal
-btnConfirm.addEventListener("click", function(event) {
-    event.preventDefault(); // Evita que el formulario se envíe
-
-    console.log("Validar NomBRE: "+ validarNombre());
-    console.log("Validar telefon: "+ validarTelefono());
-    console.log("Validar email: "+ validarEmail());
-
-    //Validacion de los campos
-    if (validarNombre() && validarApellido() && validarTelefono() && validarEmail()) {
-        guardarDatos(nombre, apellido, txtTelefono, email);
-        limpiarCampos();
-    } else {
-        console.log("Por favor, ingresa valores válidos en los campos.");
-    }
-});
+//Esto es de las validaciones
+let alertValidacionesTexto = document.getElementById("alertValidacionesTexto");
+let alertValidaciones = document.getElementById("alertValidaciones");
 let isValid = true;
+//-------------------------------------------------------
 
+// Función principal
+btnConfirm.addEventListener("click", function (event) {
+  event.preventDefault(); // Evita que el formulario se envíe
+
+  isValid = true;
+  clearTimeout(idTimeout);
+  //quita los textos si se cumple todo
+  alertValidacionesTexto.innerHTML = "";
+  //quita el se debe escribir.. estableciendolo en none fijo
+  alertValidaciones.style.display = "none";
+  //   nombre.value = nombre.value.trim(); HACE LO MISMO QUE BLUR
+  let lista = "Los siguientes campos deben ser llenados correctamente:<ul>";
+
+  //SI LA FUNCION DA FALSE SE QUITA EL BORDE SI DA TRUE SE PONE EL ALERT Y EL BORDE ROJO
+  //validacion de campo nombre
+  !validarNombre()
+    ? ((nombre.style.border = "solid thin red"),
+      (lista += "<li>Se debe escribir un nombre de 2 o mas letras</li>"),
+      (alertValidaciones.style.display = "block"),
+      (isValid = false))
+    : (nombre.style.border = "");
+  //validacion de campo nombre
+  !validarApellido()
+    ? ((apellido.style.border = "solid thin red"),
+      (lista += "<li>Se debe escribir un apellido de 2 o mas letras</li>"),
+      (alertValidaciones.style.display = "block"),
+      (isValid = false))
+    : (apellido.style.border = "");
+
+  //validacion campo Password
+  !validarPassword()
+    ? ((password.style.border = "solid thin red"),
+      (lista += "<li>Comprueba tu contraseña</li>"),
+      (alertValidaciones.style.display = "block"),
+      (isValid = false))
+    : (password.style.border = "");
+
+  //validacion imagen
+  !validarImagen()
+    ? ((imagen.style.border = "solid thin red"),
+      (lista += "<li>Agrega una imagen</li>"),
+      (alertValidaciones.style.display = "block"),
+      (isValid = false))
+    : (imagen.style.border = "");
+
+  //validacion de campo precio
+  !validarTelefono()
+    ? ((txtTelefono.style.border = "solid thin red"),
+      (lista += "<li> Se debe escribir una precio valido</li>"),
+      (alertValidaciones.style.display = "block"),
+      (isValid = false))
+    : (txtTelefono.style.border = "");
+
+  //validacion de campo Email
+  !validarEmail()
+    ? ((correo.style.border = "solid thin red"),
+      (lista += "<li> Se debe escribir un email valido (ana.isabel@gmail.com)</li>"),
+      (alertValidaciones.style.display = "block"),
+      (isValid = false))
+    : (correo.style.border = "");
+
+  //validacion de campo Boleta
+  !validarBoleta()
+    ? ((boleta.style.border = "solid thin red"),
+      (lista += "<li>Ingresa un numero de boleta que contenga 10 caracteres</li>"),
+      (alertValidaciones.style.display = "block"),
+      (isValid = false))
+    : (boleta.style.border = "");
+
+  lista += "</ul>";
+  //agrega el resultado de los if al campo validaciones
+  alertValidacionesTexto.insertAdjacentHTML("beforeend", lista);
+  //quita las validaciones despues de 3 seg
+  idTimeout = setTimeout(() => {
+    alertValidaciones.style.display = "none";
+  }, 3000);
+
+  //   Codigo que se ejecuta al cumplir validaciones
+  // if (isValid) {
+
+  // }
+  //   ---------------------------------------------
+});
 
 // Funciones de validación
 
 function validarNombre() {
-    if (nombre.value.length < 2) {
-        return false;
-      }
-      return true;
+  if (nombre.value.length < 2) {
+    return false;
+  }
+  return true;
 }
 
 function validarApellido() {
   if (apellido.value.length < 2) {
-      return false;
-    }
-    return true;
+    return false;
+  }
+  return true;
 }
 
 //Validacon Telefono
 function validarTelefono() {
-    if (
-      txtTelefono.value.length == 0 ||
-      /^(\d)\1+$/.test(txtTelefono.value) ||
-      isNaN(txtTelefono.value) ||
-      txtTelefono.value.length > 10 ||
-      txtTelefono.value.length < 10
-    ) {
-      isValid = false;
-      return false;
-    }
-  
-    
+  if (
+    txtTelefono.value.length == 0 ||
+    /^(\d)\1+$/.test(txtTelefono.value) ||
+    isNaN(txtTelefono.value) ||
+    txtTelefono.value.length > 10 ||
+    txtTelefono.value.length < 10
+  ) {
+    isValid = false;
+    return false;
+  }
+
+  return true;
+}
+
+//Validar mail
+function validarEmail() {
+  //  valida el correo electrónico
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+  // Validar el correo electrónico y limpia el input si es correcto
+  if (emailRegex.test(email.value)) {
     return true;
+  } else {
+    return false;
   }
-  
-  //Validar mail
-  function validarEmail() {
-    //  valida el correo electrónico
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  
-    // Validar el correo electrónico y limpia el input si es correcto
-    if (emailRegex.test(email.value)) {
-      return true;
-    } else {
-      return false;
-    }
-  }
+}
 // Función para guardar datos en local storage
 function guardarDatos(nombre, txtTelefono, email) {
-    const datos = {
-        nombre: nombre.value,
-        apellido : apellido.value,
-        telefono: txtTelefono.value,
-        email: email.value
-    };
+  const datos = {
+    nombre: nombre.value,
+    apellido: apellido.value,
+    telefono: txtTelefono.value,
+    email: email.value,
+  };
 
-    localStorage.setItem('datos', JSON.stringify(datos));
+  localStorage.setItem("datos", JSON.stringify(datos));
 }
 
 // Función para limpiar campos
 function limpiarCampos() {
-    document.getElementById("name").value = '';
-    document.getElementById("Nombre").value = '';
-    document.getElementById("telefono").value = '';
-    document.getElementById("email").value = '';
-    document.getElementById("boleta").value = '';
+  document.getElementById("name").value = "";
+  document.getElementById("Nombre").value = "";
+  document.getElementById("telefono").value = "";
+  document.getElementById("email").value = "";
+  document.getElementById("boleta").value = "";
 }
