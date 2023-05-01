4b0a7f4b6e524b08e5cbd21f85739efb6db6efa8
ARTR <trejoramon141295@gmail.com>
2023-05-01 13:20:29 -0600

    Se ajusto la funcion autenticarUsuario con los registros

--- js/login.js
+++ js/login.js
@@ -1,18 +1,18 @@
 
-let txtUsuario = document.getElementById("txtUsuario");
-let txtPasword = document.getElementById("txtPasword");
+let txtUsuario = document.getElementById("txtCorreo");
+let txtPasword = document.getElementById("password");
 
-const btnIngresar = document.getElementById("btnIngresar");
+const btnIngresar = document.getElementById("btnEnviar");
 
 //Usuarios Inventados para verificar la funcion
-let usuarios = [
+/*let usuarios = [
     { nombre: "usuario1", password: "password1" },
     { nombre: "usuario2", password: "password2" },
     { nombre: "usuario3", password: "password3" },
 ];
 
-//para obtener los ususarios del localStorage
-localStorage.setItem("usuarios", JSON.stringify(usuarios));  
+//para obtener los usuarios del localStorage
+localStorage.setItem("usuarios", JSON.stringify(usuarios));*/  
 
 //Botton de Ingresar
 btnIngresar.addEventListener("click", function(event) {
@@ -23,18 +23,18 @@ btnIngresar.addEventListener("click", function(event) {
 
 //funcion autenticar Usuario Ingresado 
 function autenticarUsuario(usuario, password) {
-    const usuarios = JSON.parse(localStorage.getItem("usuarios"));
+    const usuarios = JSON.parse(localStorage.getItem("registros"));//
   
     if (usuarios) {
       //Vaidacion estricta de los datos  
-      const usuarioValido = usuarios.find(u => u.nombre === usuario && u.password === password);
+      const usuarioValido = usuarios.find(u => u.correo === usuario && u.password === password);
       
       if (usuarioValido) {
-        const usuarioPrueba = {
+        const usuarioVerificado = {
           txtUsuario: txtUsuario.value,
           txtPasword: txtPasword.value
         };        
-        localStorage.setItem("usuarioPrueba", JSON.stringify(usuarioPrueba));        
+        localStorage.setItem("usuarioVerificado", JSON.stringify(usuarioVerificado));        
         console.log("Usuario autenticado correctamente");
         
         //miFormularioLogin.reset();
