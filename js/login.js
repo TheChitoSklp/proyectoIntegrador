ad3ac68e10b03a19b60c6ef6fcc7aa8aa36fe8dd
dasd <dasd2405@gmail.com>
2023-05-02 01:19:01 -0600

    Se agregan validaciones con local storage

--- js/login.js
+++ js/login.js
@@ -1,17 +1,8 @@
 
 let txtUsuario = document.getElementById("email");
 let txtPasword = document.getElementById("password");
-
-const btnIngresar = document.getElementById("btnIngresar");
-
-//Botton de Ingresar
-btnIngresar.addEventListener("click", function(event) {
-    event.preventDefault();
-
-
-    //se lleva los datos de los campos
-    autenticarUsuario(txtUsuario.value, txtPasword.value);
- });
+const formulario = document.getElementById("formSend");
+const inputs = document.querySelectorAll("#formSend input");
 
 //funcion autenticar Usuario Ingresado 
 function autenticarUsuario(usuario, password) {
@@ -28,12 +19,82 @@ function autenticarUsuario(usuario, password) {
         };        
         localStorage.setItem("usuarioVerificado", JSON.stringify(usuarioVerificado));        
         console.log("Usuario autenticado correctamente");
-        
+        document.querySelector("#dbtn .formulario__input-error").classList.remove("formulario__input-error-activo");
+        formulario.reset();
         //miFormularioLogin.reset();
       } else {
         console.log("Usuario no válido");
+        document.querySelector("#dbtn .formulario__input-error").classList.add("formulario__input-error-activo");
       }
     } else {
       console.log("No hay usuarios almacenados");
+      document.querySelector("#dbtn .formulario__input-error").classList.add("formulario__input-error-activo");
     }
 }
+
+
+
+
+const campos = {
+	password: false,
+	correo: false
+}
+
+const validaciones = {
+    password: /^.{6,15}$/, 
+	correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/
+}
+
+const validar = (e) => {
+    switch (e.target.name){
+        case "email":
+            if(validaciones.correo.test(e.target.value)){
+                document.getElementById("demail").classList.remove("formulario_incorrecto");
+                document.getElementById("demail").classList.add("formulario_correcto");
+                document.querySelector("#flogin .formulario__input-error").classList.remove("formulario__input-error-activo");
+                campos.correo = true;
+                console.log(campos.correo);
+            } else{
+                document.getElementById("demail").classList.add("formulario_incorrecto");
+                document.querySelector("#flogin .formulario__input-error").classList.add("formulario__input-error-activo")
+                campos.correo = false;
+            }
+        break;
+
+        case "password":
+            if(validaciones.password.test(e.target.value)){
+                document.getElementById("dpassword").classList.remove("formulario_incorrecto");
+                document.getElementById("dpassword").classList.add("formulario_correcto");
+                document.querySelector("#fpassword .formulario__input-error").classList.remove("formulario__input-error-activo");
+                campos.password=true; 
+            } else{
+                document.getElementById("dpassword").classList.add("formulario_incorrecto");
+                document.querySelector("#fpassword .formulario__input-error").classList.add("formulario__input-error-activo");
+                campos.password=false; 
+            }   
+        break;
+    }
+}
+
+inputs.forEach((input) => {
+    input.addEventListener("keyup", validar);
+    input.addEventListener("blur", validar);
+})
+
+formulario.addEventListener("submit", (e) => {
+e.preventDefault();
+
+    if (campos.correo && campos.password){
+        
+        document.getElementById("demail").classList.remove("formulario_correcto");
+        document.getElementById("dpassword").classList.remove("formulario_correcto");
+        autenticarUsuario(txtUsuario.value, txtPasword.value);
+
+
+    } else{
+        document.getElementById("demail").classList.add("formulario_incorrecto");
+        document.querySelector("#flogin .formulario__input-error").classList.add("formulario__input-error-activo");
+        document.getElementById("dpassword").classList.add("formulario_incorrecto");
+        document.querySelector("#fpassword .formulario__input-error").classList.add("formulario__input-error-activo");
+    }
+})
