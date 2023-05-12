342e920797ba6a0044661924b18e0ea0ecabbf90
ernt <ernestn@ciencias.unam.mx>
2023-05-12 09:45:01 -0600

    Cambios en los insert de la base de datos

--- BaseDeDatos/scripsInsertarDatos.sql
+++ BaseDeDatos/scripsInsertarDatos.sql
@@ -1,5 +1,5 @@
 -- DMLIPNGIRLSHOP
-
+USE `ipngirlshopdb` ;
 -- Agregar categorías
 INSERT INTO categorias (nombre)
 VALUES ('Maquillaje');
@@ -19,31 +19,31 @@ VALUES ('Varios');
 -- Agregando Productos
 INSERT INTO `productos` (nombre, descripcion, precio, imagen, cantidad, categorias_id) 
 VALUES ('Anillo Amistad','Los amigos son amigos para siempre y por siempre',
-		250,'No hay Imagen',73,1 );
+		250,'imagen1.jpg',73,1 );
 INSERT INTO `productos` (nombre, descripcion, precio, imagen, cantidad, categorias_id) 
 VALUES ('Botas chillonas','Para el mejor cocinero del mundo',
-		100,'No hay Imagen',693,4 );
+		100,'imagen2.jpg',693,4 );
         INSERT INTO `productos` (nombre, descripcion, precio, imagen, cantidad, categorias_id) 
 VALUES ('Sandalias aerodinámicas','Son aerodinámicas ',
-		300,'No hay Imagen',103,3 );
+		300,'imagen3.jpg',103,3 );
         INSERT INTO `productos` (nombre, descripcion, precio, imagen, cantidad, categorias_id) 
 VALUES ('Capa de invisivilidad ','Te hace menos visible para otros ',
-		340,'No hay Imagen',73,5 );
+		340,'imagen4.jpg',73,5 );
         INSERT INTO `productos` (nombre, descripcion, precio, imagen, cantidad, categorias_id) 
 VALUES ('Pastilla de chiquitolina  ','Olvidate de las partículas PIM',
-		215,'No hay Imagen',83,6 );
+		215,'imagen5.jpg',83,6 );
 -- Agregando usuario 
 
  INSERT INTO `usuarios` (`nombre`,`apellido`,`password`,`boleta`,`correo`,`telefono`,`rol`,`foto`) 
-VALUES ('Ernesto ','Nieves','holaquehace',1234111111,'ernm@gmail.com',1111111111,'vendedor','No hay foto');
+VALUES ('Ernesto ','Nieves','holaquehace',1234111111,'ernm@gmail.com',1111111111,'vendedor','foto.jpg');
 INSERT INTO `usuarios` (`nombre`,`apellido`,`password`,`boleta`,`correo`,`telefono`,`rol`,`foto`)  
-VALUES ('Daniel','Montoya','constra',2928399999,'corre@correo.com',1111111234,'comprador','not image');
+VALUES ('Daniel','Montoya','constra',2928399999,'corre@correo.com',1111111234,'comprador','foto1.jpg');
 INSERT INTO `usuarios` (`nombre`,`apellido`,`password`,`boleta`,`correo`,`telefono`,`rol`,`foto`) 
- VALUES ('Adriana', 'Ibarra', 'Pa$$word.1', 9876543211,'adimania@gmail.com',1234567890,'vendedor','No hay imagen');
+ VALUES ('Adriana', 'Ibarra', 'Pa$$word.1', 9876543211,'adimania@gmail.com',1234567890,'vendedor','foto2.jpg');
 INSERT INTO `usuarios`(`nombre`,`apellido`,`password`,`boleta`,`correo`,`telefono`,`rol`,`foto`) 
- VALUES ('juan pablo','avina araiza','C@rton54',1234567891,'chito.pepsi@gmail.com',222222222,'comprador','fotito de persona');
+ VALUES ('juan pablo','avina araiza','C@rton54',1234567891,'chito.pepsi@gmail.com',222222222,'comprador','foto3.jpg');
 INSERT INTO `usuarios`(`nombre`,`apellido`,`password`,`boleta`,`correo`,`telefono`,`rol`,`foto`) 
- VALUES ('Alberto ','Trejo','C@rton54',1234567891,'albrt@gmail.com',222222222,'comprador','fotito ');
+ VALUES ('Alberto ','Trejo','C@rton54',1234567891,'albrt@gmail.com',222222222,'comprador','foto4.jpg');
 
 
 SELECT * FROM ipngirlshopdb.categorias;
